b0fb7289eae26723f0d957c70fa83ce1a8e44bab
dragos_06 <dragos_06@LAPTOP-JHDSFSHH.localdomain>
2026-05-05 23:28:23 +0100

    test

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,14 @@
 function pad (n) {
   return String(n).padStart(2, '0')
 }
+function formatLime (ms) {
+  const totalSeconds = Math.floor(ms / 1000)
+  const minutes = Math.floor(totalSeconds / 60)
+  const seconds = totalSeconds % 60
+  const centiseconds = Math.floor((ms % 1000) / 10)
+  return `${pad(minutes)}:${pad(seconds)}.${pad(centiseconds)}`
+}
+
 
 function formatTime (ms) {
   const totalSeconds = Math.floor(ms / 1000)
